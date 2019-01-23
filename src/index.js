fa0fac455f84860e3023e8d514e30099cb002e2e
Shamsheer Ahamed <sham@Shamsheers-MacBook-Pro.local>
2019-01-23 13:48:26 -0800

    Added App and Routes

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import CalendarContainer from './containers/calendar-container';
 import * as serviceWorker from './serviceWorker';
 import './bootstrap.css';
+import Routes from './routes';
 
-ReactDOM.render(<CalendarContainer />, document.getElementById('root'));
+ReactDOM.render(<Routes />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './containers/App';
+import CalendarContainer from './containers/calendar-container';
+
+export default () => (
+  <App>
+    <CalendarContainer />
+  </App>
+)
